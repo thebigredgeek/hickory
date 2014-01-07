4ea1637dd08fce7a789978fb01c80a488c3a4195
Andrew E. Rhyne <rhyneandrew@gmail.com>
2014-01-06 16:14:42 -0800

    added proper compiler

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
-all: main.o processor.o
-	g++ processor.o main.o -o hickory
+rebuild: clean all
+
+all: compiler.o processor.o main.o
+	g++ compiler.o processor.o main.o -o hickory
 
 main.o:
 	g++ src/main.cpp -c
 
 processor.o:
 	g++ src/processor.cpp -c
+
+compiler.o:
+	g++ src/compiler.cpp -c
 
 test: all
 	./hickory programs/add.ssc
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,27 @@
 #include "processor.h"
+#include "configuration.h"
+#include "compiler.h"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
   if(argc > 1){
 
-    int instructions [100],
-        counter = 0;
+    int instructions [MEMSIZE];
 
-    std::string temp,
-           path(argv[1]);
+    std::string path(argv[1]);
 
-    std::ifstream fileStream;
-    std::string::iterator end_pos;
-    
-    fileStream.open(path);
-
+    Compiler compiler;
     Processor VM;
 
-    if(fileStream.is_open()){
+    compiler.compile(instructions, path);
 
-      while(!fileStream.eof()){
-      
-        getline(fileStream,temp);
-
-        if(temp.find("#") != std::string::npos){ //parse out comments
-          temp = temp.substr(0, temp.find("#")); 
-        }
-        
-        end_pos = std::remove(temp.begin(), temp.end(), ' ');
-        temp.erase(end_pos, temp.end());
-
-        instructions[counter] = atoi(temp.c_str());
-
-        counter ++;
-      }
-
-      VM.load(instructions, 10);
-      VM.exec();
-    }
+    VM.load(instructions, 10);
+    VM.exec();
+    
   }
   return 0;
 }
